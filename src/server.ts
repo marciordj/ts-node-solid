126b0d132e13b61b7748fe797220ace6cba91ab1
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2024-01-01 11:09:30 -0300

    chore: add prisma

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import { app } from './app';
 import { env } from './env';
 
 app.listen({
-  host: '0.0.0.0',
-  port: env.PORT
+	host: '0.0.0.0',
+	port: env.PORT
 }).then(() => {
-  console.log(`Server running on ${env.PORT}`);
+	console.log(`Server running on ${env.PORT}`);
 });

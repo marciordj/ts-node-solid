c87478e96eeece6d59e34b7d3f7bcd918207d068
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2024-06-11 23:56:40 -0300

    feat: add authtenticate usecase

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -1,9 +1,34 @@
-import { UsersRepository } from "@/repositories/users-repository";
+import { UsersRepository } from '@/repositories/users-repository';
+import { InvalidCredentialsError } from './errors/invalid-credentials-error';
+import { compare } from 'bcryptjs';
+import { User } from '@prisma/client';
 
+interface AuthenticateUseCaseRequest {
+	email: string,
+	password: string
+}
+
+interface AuthenticateUseCaseResponse {
+	user: User
+}
 export class Autenticate {
 	constructor(private readonly userRepository: UsersRepository) {}
 
-	public async execute(): {
-		
+	public async execute({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+		const user = await this.userRepository.findByEmail(email);
+
+		if (!user) {
+			throw new InvalidCredentialsError();
+		}
+
+		const doesPasswordMaches = await compare(password, user.password_hash);
+
+		if (!doesPasswordMaches) {
+			throw new InvalidCredentialsError();
+		}
+
+		return {
+			user
+		};
 	}
 }

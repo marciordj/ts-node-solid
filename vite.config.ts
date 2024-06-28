0d987bee3f380ddb33163627f8a5d5ca1f2395cd
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2024-06-28 12:21:57 -0300

    chore: add folders exclude in vitest confgs

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,17 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [tsconfigPaths()]
+	plugins: [tsconfigPaths()],
+	test: {
+		environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+		exclude: [
+			"node_modules",
+      "dist",
+      ".idea",
+      ".git",
+      ".cache",
+      "build",
+      "pgdata",
+		]
+	},
 });
